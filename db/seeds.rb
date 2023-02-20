5ace91bb23eca27009729ab6d26cda749c862b05
strangeal <bajerijustice@gmail.com>
2023-02-20 09:39:59 -0800

    Add data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Greeting.create(message: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.")
+Greeting.create(message: "First, solve the problem. Then, write the code.")
+Greeting.create(message: "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.")
+Greeting.create(message: "Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away.")
+Greeting.create(message: "Optimism is an occupational hazard of programming: feedback is the treatment. ")
