1d38c77cb346f3918391e55e59b0d8b3fc01b11f
strangeal <bajerijustice@gmail.com>
2023-02-20 10:50:13 -0800

    remove logger

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { logger } from 'redux-logger';
 import greetingReducer from './greetingSlice';
 
 const store = configureStore({
   reducer: {
     message: greetingReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
